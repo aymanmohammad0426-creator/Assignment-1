0128bccded94e9e9e651c752a2d6c7e4da383619
aymanmohammad0426-creator <aymanmohammad0426@gmail.com>
2025-09-12 22:25:54 +0530

    initial commit

---
+++ Task1.py
@@ -0,0 +1,7 @@
+a=float(input("Enter the first number: "))
+b=float(input("Enter the second number: "))
+
+print("addition: ", a+b)
+print("subtraction: ", a-b)
+print("multiplication: ", a*b)
+print("division: ", a/b)
